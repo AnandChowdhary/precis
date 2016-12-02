204999d954600e6737f95d366428a20367e82e8b
Anand Chowdhary <anandchowdhary@gmail.com>
2016-12-02 19:01:22 +0530

    Make table better

--- assets/_shortn.js
+++ assets/_shortn.js
@@ -7,12 +7,14 @@ const Precis = {
 const domain = "osw.li";
 
 let shortenURL = (url) => {
-	$(".shorten-url").val("Shortening...");
+	$(".shorten-url").attr("disabled", "true");
 	$.ajax({url: Precis.create + "?url=" + url + "&creator=" + domain, success: (result) => {
+		$(".shorten-url").removeAttr("disabled");
 		$(".shorten-url").val(location.protocol + "//" + domain + "/" + result).select();
 		let urlx = url.replace("https://", "").replace("http://", "").replace("www.", "");
 		$("<tr><td>" + result + "</td><td>" + urlx + "</td><td><a href='#'>View</a></td><td><a href='#'>Open</a></td></tr>").prependTo("[data-table]");
 		$(".shorten-url").toggleClass("big");
+		$("[data-shortened]").html(parseInt($("[data-shortened]").html())+1);
 		setTimeout(() => {
 			$(".shorten-url").toggleClass("big");
 		}, 200);
@@ -44,6 +46,9 @@ $(() => {
 	$.ajax({url: Precis.analyze + "?creator=" + domain + "&type=table", success: (result) => {
 		$("[data-table]").html(result);
 	}});
+	$.ajax({url: Precis.analyze + "?creator=" + domain + "&type=recent", success: (result) => {
+		$("[data-recent]").html(result);
+	}});
 	$("#circle").circleProgress({
 		value: 0.75,
 		size: 50,
@@ -52,3 +57,9 @@ $(() => {
 });
 
 new Clipboard(".clipboard-btn");
+
+$(".btn-show").click(() => {
+	$(".table-limited").removeClass("table-limited");
+	$(".btn-show").parent().hide();
+	$(".glare").hide();
+});

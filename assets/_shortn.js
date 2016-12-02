45d789de25dc1de4f97d96ce4caf2da22e1fff58
Anand Chowdhary <anandchowdhary@gmail.com>
2016-12-02 18:27:03 +0530

    Initial commit

---
+++ assets/_shortn.js
@@ -0,0 +1,54 @@
+const Precis = {
+	create: "http://localhost/shorten.php",
+	analyze: "http://localhost/analytics.php"
+};
+
+//const domain = document.domain;
+const domain = "osw.li";
+
+let shortenURL = (url) => {
+	$(".shorten-url").val("Shortening...");
+	$.ajax({url: Precis.create + "?url=" + url + "&creator=" + domain, success: (result) => {
+		$(".shorten-url").val(location.protocol + "//" + domain + "/" + result).select();
+		let urlx = url.replace("https://", "").replace("http://", "").replace("www.", "");
+		$("<tr><td>" + result + "</td><td>" + urlx + "</td><td><a href='#'>View</a></td><td><a href='#'>Open</a></td></tr>").prependTo("[data-table]");
+		$(".shorten-url").toggleClass("big");
+		setTimeout(() => {
+			$(".shorten-url").toggleClass("big");
+		}, 200);
+	}});
+};
+
+$(".shorten-url").on("paste", () => {
+	setTimeout(() => {
+		shortenURL($(".shorten-url").val());
+	}, 1);
+});
+
+$(".shorten-form").submit((e) => {
+	shortenURL($(".shorten-url").val());
+	e.preventDefault();
+});
+
+$(() => {
+	$(".domain-name").html(domain);
+	$.ajax({url: Precis.analyze + "?creator=" + domain + "&type=shortened", success: (result) => {
+		$("[data-shortened]").html(result);
+	}});
+	$.ajax({url: Precis.analyze + "?creator=" + domain + "&type=limitleft", success: (result) => {
+		$("[data-limitleft]").html(result);
+	}});
+	$.ajax({url: Precis.analyze + "?creator=" + domain + "&type=total", success: (result) => {
+		$("[data-total]").html(result);
+	}});
+	$.ajax({url: Precis.analyze + "?creator=" + domain + "&type=table", success: (result) => {
+		$("[data-table]").html(result);
+	}});
+	$("#circle").circleProgress({
+		value: 0.75,
+		size: 50,
+		fill: "#0275d8"
+	});
+});
+
+new Clipboard(".clipboard-btn");
